814e7d3073d62905f1785fadc40ddea5bd620ed1
jerinwilson7 <jerinwilson7@gmail.com>
2023-09-22 23:23:15 +0530

    initial commit

---
+++ routes/users.js
@@ -0,0 +1,11 @@
+var express = require("express");
+var router = express.Router();
+// const USER = require("../models/user");
+
+/* GET users listing. */
+router.post("/create-user", function (req, res, next) {
+  const { name, email, password } = req.body;
+  console.log(name, email, password);
+  res.send("GET request received");
+});
+module.exports = router;
