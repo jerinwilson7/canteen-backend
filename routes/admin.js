5863bf84b43c3c1a4cdd75b3404f820c258f8ab1
jerinwilson7 <jerinwilson7@gmail.com>
2023-11-06 22:53:01 +0530

    started with admin login

--- routes/admin.js
+++ routes/admin.js
@@ -1,9 +1,15 @@
 var express = require("express");
+const { adminLogin } = require("../Services/adminAuthServices");
 var router = express.Router();
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
   res.render("index", { title: "Express" });
+});
+
+router.post("/login", async (req, res) => {
+  const response = await adminLogin(req.UserName);
+  res.json(response);
 });
 
 module.exports = router;
