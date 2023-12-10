51f15ad979a078b61cc8fd1403910e71db8f6b9b
basil4693 <basilshaji4693@gmail.com>
2023-12-11 00:21:09 +0530

    updated destination file

--- multer.js
+++ multer.js
@@ -3,7 +3,7 @@ const multer = require('multer')
 
 const storage = multer.diskStorage({
     destination:(req,file,cb)=>{
-        cb(null,'/uploads/')
+        cb(null,'uploads/')
     },
     filename: (req,file,cb)=>{
         const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
