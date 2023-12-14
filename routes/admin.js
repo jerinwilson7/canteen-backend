ec76e625561dfde5222df6fbd56e79a10205c28b
basil4693 <basilshaji4693@gmail.com>
2023-12-14 23:44:15 +0530

    added get all product

--- routes/admin.js
+++ routes/admin.js
@@ -1,6 +1,7 @@
 var express = require("express");
 const { adminLogin, adminRegister } = require("../Services/adminAuthServices");
 const { upload } = require("../multer");
+const {addFood, getAllProducts } = require("../Services/productServices");
 var router = express.Router();
 
 /* GET home page. */
@@ -22,13 +23,39 @@ router.post("/login", async (req, res) => {
 
 router.post('/add-product',upload.single('file'), async(req,res)=>{ 
   try {
-    const { name, category, description, quantity, price } = req.body;
 
-    console.log(name,category,description,quantity,price)
+    const { name,title, category, description, quantity, price } = req.body;
+
+    const file = req.file;
+
+
+    const response = await addFood({
+      name,
+      title,
+      category,
+      description,
+      quantity,
+      price,
+      foodImage: {
+        filename: file.filename, 
+        path: file.path,
+      },
+    });
+
+    console.log(response)
   } catch (error) {
     console.log(error)
   }
 })
+
+router.get('/get-all-products',async(req,res)=>{
+  console.log('getprod')
+  const response = await getAllProducts()
+  res.send(response)
+  console.log(response)
+})
+
+
 
 module.exports = router;
    
