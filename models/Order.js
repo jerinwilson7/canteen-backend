7cb4c7fe8cbfc64cf4f0d7758dd63ffa2d7d5772
basil4693 <basilshaji4693@gmail.com>
2024-01-27 23:51:07 +0530

    modified order model

--- models/Order.js
+++ models/Order.js
@@ -6,34 +6,25 @@ const orderSchema = new mongoose.Schema({
     ref: "User",
     required: true,
   },
-  products: [
+  items: [
     {
-      name: {
-        type: String,
-        required: true,
-      },
-      quantity: {
-        type: Number,
-        required: true,
-      },
-      price: {
-        type: Number,
-        required: true,
-      },
-      image: {
-        type: String,
-        required: true,
-      },
-    },
-  ],
-  totalPrice: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Cart",
+    required: true,
+  },
+],
+
+  
+  totalAmount: {
     type: Number,
     required: true,
   },
-  PaymentMethod: {
-    type: String, 
-    required: true, 
+
+  paymentId: {
+    type: String,
+    required: true,
   },
+
   createdAt: {
     type: Date,
     default: Date.now(),
