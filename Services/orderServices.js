465954bfbce1dc0f90f20e52aec9fc29ae05f4a5
jerinwilson7 <jerinwilson7@gmail.com>
2024-02-26 01:08:30 +0530

    updated order services

--- Services/orderServices.js
+++ Services/orderServices.js
@@ -9,14 +9,14 @@ const placeOrder = async (data, userEmail) => {
       const { paymentId } = data;
       const user = await User.findOne({ email: userEmail });
       let userId = user.id;
-      let userName = user.name
+      let userName = user.name;
       console.log(userId);
 
       const cartResponse = await getCartItems({ userEmail });
-      const cartItems = cartResponse.data.cartItems
-      const totalAmount = cartResponse.data.metaData.itemsTotal
+      const cartItems = cartResponse.data.cartItems;
+      const totalAmount = cartResponse.data.metaData.itemsTotal;
 
-      const orderItems = cartItems.map(cartItem => {
+      const orderItems = cartItems.map((cartItem) => {
         return {
           name: cartItem.food.name,
           quantity: cartItem.quantity,
@@ -25,21 +25,18 @@ const placeOrder = async (data, userEmail) => {
         };
       });
 
-   
-   
-
       const orderObject = {
         user: userId,
-        email:userEmail,
-        userName:userName,
+        email: userEmail,
+        userName: userName,
         items: orderItems,
         totalAmount: totalAmount,
         paymentId: paymentId,
       };
       await Order.create(orderObject).then((res) => {
-        deleteCart({userEmail})
+        deleteCart({ userEmail });
         const orderPromise = {
-          status:true,
+          status: true,
           data: res,
           message: "order success",
         };
@@ -48,48 +45,44 @@ const placeOrder = async (data, userEmail) => {
     });
   } catch (error) {
     console.log("placeOrder error : " + error);
-    return  {
-      status:false,
+    return {
+      status: false,
       error: error,
       message: "order failed",
     };
   }
 };
 
-const getUserOrders = async ( userEmail ) => {
+const getUserOrders = async (userEmail) => {
   try {
-    console.log(userEmail)
+    console.log(userEmail);
     return new Promise(async (resolve, reject) => {
-      const user = await User.findOne({ email:userEmail });
-      
+      const user = await User.findOne({ email: userEmail });
+
       let userId = user.id;
-      ;
 
       const order = await Order.findOne({ user: userId });
-      console.log("getorder")
-      const food = await Cart.findOne({_id:order.items})
+      console.log("getorder");
+      const food = await Cart.findOne({ _id: order.items });
 
-      console.log(food)
-      console.log(order.items.name)
+      console.log(food);
+      console.log(order.items.name);
       resolve(order);
     });
   } catch (error) {
-    console.log(error); 
+    console.log(error);
   }
 };
 
-const getAllOrders = async()=>{
+const getAllOrders = async () => {
   try {
-
-    return new Promise(async(resolve,reject)=>{
-      const allOrders = await Order.find()
-      resolve(allOrders)
-        })
-    
+    return new Promise(async (resolve, reject) => {
+      const allOrders = await Order.find();
+      resolve(allOrders);
+    });
   } catch (error) {
-    console.log("all order error :"+ error)
+    console.log("all order error :" + error);
   }
-}
+};
 
-module.exports = { placeOrder ,getUserOrders,getAllOrders};
- 
+module.exports = { placeOrder, getUserOrders, getAllOrders };
